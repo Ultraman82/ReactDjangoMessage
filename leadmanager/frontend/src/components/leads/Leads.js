9b06b91454e733788f73e45dcc9a2815e0a81636
Ultraman82 <exelcior99@gmail.com>
2020-05-16 01:17:02 -0700

    Redux backbone is set

--- leadmanager/frontend/src/components/leads/Leads.js
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -1,13 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { getLeads } from "../../actions/leads";
 
 export class Leads extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Leads List</h1>
-            </div>
-        )
-    }
+  static PropTypes = {
+    leads: PropTypes.array.isRequired,
+  };
+  render() {
+    return (
+      <div>
+        <h1>Leads List</h1>
+      </div>
+    );
+  }
 }
 
-export default Leads
+const mapStateToProps = (state) => ({
+  leads: state.leads.leads,
+});
+export default connect(mapStateToProps)(Leads);
