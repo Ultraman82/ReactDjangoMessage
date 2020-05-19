b2abf82882e727b1c699279185eb649a49f417aa
Ultraman82 <exelcior99@gmail.com>
2020-05-19 16:00:36 -0700

    Login, Register components added

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,11 +1,19 @@
 import React, { Component, Fragment } from "react";
 import ReactDOM from "react-dom";
-
+import {
+  HashRouter as Rtourer,
+  Route,
+  Switch,
+  Redirect,
+  Router,
+} from "react-router-dom";
 import { Provider as AlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
 import Header from "./layout/Header";
 import Dashboard from "./leads/Dashboard";
 import Alerts from "./layout/Alerts";
+import Login from "./accounts/Login";
+import Register from "./accounts/Register";
 
 import { Provider } from "react-redux";
 import store from "../store";
@@ -21,13 +29,19 @@ class App extends Component {
     return (
       <Provider store={store}>
         <AlertProvider template={AlertTemplate} {...alertOptions}>
-          <Fragment>
-            <Header></Header>
-            <Alerts></Alerts>
-            <div className="container">
-              <Dashboard />
-            </div>
-          </Fragment>
+          <Router>
+            <Fragment>
+              <Header></Header>
+              <Alerts></Alerts>
+              <div className="container">
+                <Switch>
+                  <Route exact path="/" component={Dashboard} />
+                  <Route exact path="/register" component={Register} />
+                  <Route exact path="/login" component={Login} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
         </AlertProvider>
       </Provider>
     );
